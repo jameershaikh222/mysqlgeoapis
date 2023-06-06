01315d889b3d9f49e210f2188aac04c02a6ee11c
Jameer Shaikh <jameer@mantle-labs.com>
2023-06-06 20:55:03 +0530

    git commit

---
+++ mysqljpa/src/main/java/com/testjpa/mysqljpa/MysqljpaApplication.java
@@ -0,0 +1,15 @@
+package com.testjpa.mysqljpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqljpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqljpaApplication.class, args);
+		
+		System.out.println("MySQLJPAGeo Demo...!");
+	}
+
+}
